de98615b32c3cfd1dda261d87550832d21045e7c
Vlad-31312 <v_dimitrovich@kbtu.kz>
2026-02-07 14:56:04 +0600

    Create Practice2 folder

---
+++ Practice2/Loops/While loops 4.py
@@ -0,0 +1,5 @@
+while True:
+    command = input("Введите 'exit' для выхода: ")
+    if command == "exit":
+        break
+print("Программа завершена")
---
+++ Practice2/Loops/While loops 5.py
@@ -0,0 +1,9 @@
+n = int(input("Введите число n: "))
+i = 1
+total = 0
+
+while i <= n:
+    total += i
+    i += 1
+
+print("Сумма чисел от 1 до", n, "равна", total)
