44e8e1beedbc167eb3e05089dc16ae05194c92f3
Vlad-31312 <v_dimitrovich@kbtu.kz>
2026-02-18 12:44:27 +0600

    Add Practice 3 with four Python folders

---
+++ Practice 3/Functions/return_values.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def circle_area(radius):
+    pi = 3.14159
+    return pi * radius ** 2
+
+def is_even(number):
+    return number % 2 == 0
+
+sum_result = add(5, 3)
+area_result = circle_area(4)
+even_check = is_even(10)
+
+print(sum_result)
+print(area_result)
+print(even_check)
